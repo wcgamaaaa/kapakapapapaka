bb2f5d96b5550833b9cb241ed04669b9a800a615
wcgamaaaa <wagnerjuniorca2007@gmail.com>
2024-10-09 12:19:16 -0300

    czczc

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function criarCartao(Categoria,Pergunta,Resposta){
- let container = document.getElementById('container');
- let cartao = document.createElement('articicle');
- cartao.className = 'cartao';
- cartao.innerHTML =  `
- <div class="conteudo-cartao">
- <h3>Categorias</h3>
- <div class="pergunta-cartao">
+  let container = document.getElementById('container');
+  let cartao = document.createElement('articicle');
+   cartao.className = 'cartao';
+   cartao.innerHTML =  `
+  <div class="conteudo-cartao">
+  <h3>Categorias</h3>
+  <div class="pergunta-cartao">
     <p>pergunta</p>
- </div>
- <div class="resposta-cartao">
+  </div>
+  <div class="resposta-cartao">
     <p>resposta</p>
- </div>
+  </div>
 </div>
 `
 container.appendChild(cartao);
