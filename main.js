3c9003828be99d9395e4f492e39651e91b648ccd
wcgamaaaa <wagnerjuniorca2007@gmail.com>
2024-10-16 11:26:45 -0300

    adada

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 function criarCartao(Categoria,Pergunta,Resposta){
   let container = document.getElementById('container');
   let cartao = document.createElement('articicle');
-   cartao.className = 'cartao';
-   cartao.innerHTML =  `
-  <div class="conteudo-cartao">
-  <h3>Categorias</h3>
+  cartao.className = 'cartao';
+  cartao.innerHTML =  `
+  '  <div class="conteudo-cartao">
+     <h3>Categorias</h3>
   <div class="pergunta-cartao">
-    <p>pergunta</p>
+     <p>pergunta</p>
   </div>
   <div class="resposta-cartao">
     <p>resposta</p>
-  </div>
+    </div>
 </div>
-`
-container.appendChild(cartao);
+   `
+   container.appendChild(cartao);
 }
