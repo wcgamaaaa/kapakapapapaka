b15c5a80315aca8400dbd19fbcd0bf73805fe5ea
wcgamaaaa <wagnerjuniorca2007@gmail.com>
2024-10-29 08:32:22 -0300

    tyi

--- projeto2.js
+++ projeto2.js
@@ -16,7 +16,7 @@ function criarCartao(Categoria,Pergunta,Resposta){
      container.appendChild(cartao);
   }
   function mostraResposta(card){
-    const resposta = document.querySelector('.resposta-cartao')
+    const resposta = card.querySelector('.resposta-cartao')
     if(resposta.style.display === 'none'){
         resposta.style.display = 'block';
     }
