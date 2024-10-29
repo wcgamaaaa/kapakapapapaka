f5c74cd76b9a1fd1563806c71b789f6fae2c28e3
wcgamaaaa <wagnerjuniorca2007@gmail.com>
2024-10-29 08:19:14 -0300

    kh

--- projeto2.js
+++ projeto2.js
@@ -15,4 +15,12 @@ function criarCartao(Categoria,Pergunta,Resposta){
      `
      container.appendChild(cartao);
   }
-  
+  function mostraResposta (){
+    const resposta = document.querySelector('.resposta-cartao')
+    if(resposta.style.display === 'none'){
+        resposta.style.display = 'block';
+    }
+    else{
+        resposta.style.display = 'none';
+    }
+  }
